c26a0114c2b762693fffbaca42373221794f9d33
14ic7 <14ic7@queensu.ca>
2017-02-12 14:26:06 -0500

    RTS + VR camera setup

---
+++ Boo/Assets/Scripts/DisableMirror.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableMirror : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForEndOfFrame ();
+		UnityEngine.VR.VRSettings.showDeviceView = false;
+	}
+
+}
