24da95dcd90f4ec410efbfd899825e502a8dad02
14ic7 <14ic7@queensu.ca>
2017-03-11 18:31:23 -0500

    Added flashlight power mechanic. Added particle FX for vacuum

--- Boo/Assets/Scripts/FlashlightController.cs
+++ Boo/Assets/Scripts/FlashlightController.cs
@@ -5,6 +5,9 @@ public class FlashlightController : MonoBehaviour {
 
 	new FlashlightCollider light;
 
+	private float flashlightPower = 100.0f;
+	private float flashlightDrain = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 		light = transform.GetChild(0).GetComponent<FlashlightCollider>();
@@ -12,11 +15,20 @@ public class FlashlightController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKey(KeyCode.L)) {
-			Debug.Log ("flashlight is on");
-			light.turnOn();
+		if ((OVRInput.Get (OVRInput.Button.SecondaryIndexTrigger) || Input.GetKey (KeyCode.L)) && (flashlightPower > 0.0f)) {
+			Debug.Log (flashlightPower);
+			light.turnOn ();
+			flashlightPower = Mathf.Clamp (flashlightPower - flashlightDrain, 0.0f, 100.0f);
+		} else if ((OVRInput.Get (OVRInput.Button.SecondaryIndexTrigger) || Input.GetKey (KeyCode.L)) && (flashlightPower <= 0.0f)) {
+			light.turnOff ();
+			flashlightPower = Mathf.Clamp (flashlightPower, 0.0f, 100.0f);
 		} else {
-			light.turnOff();
+			light.turnOff ();
 		}
 	}
+
+	public void AddPower (float power) {
+		flashlightPower = Mathf.Clamp(flashlightPower + power, 0.0f, 100.0f);
+	}
+
 }
--- Boo/Assets/Scripts/VacuumController.cs
+++ Boo/Assets/Scripts/VacuumController.cs
@@ -5,18 +5,24 @@ public class VacuumController : MonoBehaviour {
 
 	new VacuumCollider vacuum;
 
+	GameObject vfx;
+
 	// Use this for initialization
 	void Start () {
 		vacuum = transform.GetChild (0).GetComponent<VacuumCollider> ();
+		vfx = GameObject.Find ("Vacuum FX");
+		vfx.SetActive (false);
 		vacuum.turnOff ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (OVRInput.Get (OVRInput.Button.PrimaryIndexTrigger) == true || Input.GetKey(KeyCode.F)) {
+		if (OVRInput.Get (OVRInput.Button.PrimaryIndexTrigger) || Input.GetKey(KeyCode.F)) {
 			vacuum.turnOn ();
+			vfx.SetActive (true);
 		} else {
 			vacuum.turnOff ();
+			vfx.SetActive (false);
 		}
 	}
 		
