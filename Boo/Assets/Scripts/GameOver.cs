1508b5ceb2ac480fb233ba03268404015710fd77
14ic7 <14ic7@queensu.ca>
2017-03-22 18:15:28 -0400

    Added win/lose screens for VR

--- Boo/Assets/Scripts/GameOver.cs
+++ Boo/Assets/Scripts/GameOver.cs
@@ -10,7 +10,7 @@ public class GameOver : MonoBehaviour {
 		Color curColor = this.GetComponent<Image>().color;
 		float alphaDiff = Mathf.Abs (curColor.a - 1.0f);
 			if (alphaDiff > 0.0001f) {
-				curColor.a = Mathf.Lerp (curColor.a, 1.0f, 0.5f * Time.deltaTime);
+				curColor.a = Mathf.Lerp (curColor.a, 0.8f, 0.5f * Time.deltaTime);
 				this.GetComponent<Image>().color = curColor;
 			}
 	}
