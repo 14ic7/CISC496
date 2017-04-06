35f73fffca1d9e1543ce698eaed4e66bfd7453bb
14ic7 <14ic7@queensu.ca>
2017-04-05 23:57:01 -0400

    Added VR tutorial scene

---
+++ Boo/Assets/Scripts/SmoothFloat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFloat : MonoBehaviour {
+
+	public float amplitude;
+	public float speed;
+
+	private float yStart;
+
+	// Use this for initialization
+	void Start () {
+		yStart = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (this.transform.position.x, yStart + amplitude * Mathf.Sin (speed * Time.time), this.transform.position.z);
+
+	}
+}
--- Boo/Assets/Scripts/VRTitle.cs
+++ Boo/Assets/Scripts/VRTitle.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class VRTitle : MonoBehaviour {
 
@@ -12,6 +13,9 @@ public class VRTitle : MonoBehaviour {
 	GameObject exit;
 	GameObject tutorial;
 	GameObject reaperModel;
+	GameObject playText;
+	GameObject exitText;
+	GameObject tutorialText;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +24,9 @@ public class VRTitle : MonoBehaviour {
 		exit = GameObject.Find ("Quit_Tombstone (Selected)");
 		tutorial = GameObject.Find ("avatar-static_v2 (Selected)");
 		reaperModel = GameObject.Find ("avatar-static_v2");
+		playText = GameObject.Find ("Start Game");
+		exitText = GameObject.Find ("Quit Game");
+		tutorialText = GameObject.Find ("Tutorial");
 	}
 	
 	// Update is called once per frame
@@ -40,19 +47,33 @@ public class VRTitle : MonoBehaviour {
 			exit.SetActive (false);
 			tutorial.SetActive (false);
 			reaperModel.SetActive (true);
+			playText.SetActive (true);
+			exitText.SetActive (false);
+			tutorialText.SetActive (false);
 		} else if (selected == 1) {
 			exit.SetActive (true);
 			play.SetActive (false);
 			tutorial.SetActive (false);
 			reaperModel.SetActive (true);
+			exitText.SetActive (true);
+			playText.SetActive (false);
+			tutorialText.SetActive (false);
 		} else if (selected == 2) {
 			tutorial.SetActive (true);
 			play.SetActive (false);
 			exit.SetActive (false);
 			reaperModel.SetActive (false);
+			tutorialText.SetActive (true);
+			playText.SetActive (false);
+			exitText.SetActive (false);
 		}
 		if (OVRInput.GetDown (OVRInput.Button.One) && (selected == 0)) {
-
+			Destroy (GameObject.Find ("BGM"));
+			SceneManager.LoadScene ("VR");
+		} else if (OVRInput.GetDown (OVRInput.Button.One) && (selected == 1)) {
+			Application.Quit ();
+		} else if (OVRInput.GetDown (OVRInput.Button.One) && (selected == 2)) {
+			SceneManager.LoadScene ("VR Tutorial");
 		}
 	}
 }
